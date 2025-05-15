37c57968267c854e314d787a8da8ebb865fbdc05
Davi A Alves <daviih2000@gmail.com>
2025-05-15 09:38:18 -0300

    feat: adicionar funcionalidade de busca e inserção de tarefas na lista

--- index.js
+++ index.js
@@ -8,3 +8,30 @@ function fecharModal() {
   criarTarefa.classList.remove("active");
 }
 
+function buscarTarefas() {
+  fetch("http://localhost:3000/tarefas")
+    .then((res) => res.json())
+    .then((res) => {
+      inserirTarefas(res);
+    });
+}
+buscarTarefas();
+
+function inserirTarefas(listaDeTarefas) {
+  if (listaDeTarefas.length > 0) {
+    lista.innerHTML = "";
+    listaDeTarefas.map((tarefa) => {
+      lista.innerHTML += `
+      <li>
+            <h5>${tarefa.titulo}</h5>
+            <p>
+              ${tarefa.descricao}
+            </p>
+            <div class="actions">
+              <box-icon name="trash" size="sm"></box-icon>
+            </div>
+          </li>
+      `;
+    });
+  }
+}
